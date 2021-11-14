2588819f1ce55467d9d4e485e2e63da62d3263e7
EugenAchtzehn <eugene1826@gmail.com>
2021-11-15 03:38:22 +0800

    first built... not even a prototype.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container-fluid">
+        <header class="row mt-4 text-center">
+            <div class="col-sm-6 mr-auto">Travel Taiwan</div>
+            <div class="col-sm-6 row">
+                <div class="col-4">首頁</div>
+                <div class="col-4">景點導覽</div>
+                <div class="col-4">觀光活動</div>
+            </div>
+        </header>
+        <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+        <HelloWorld msg="Welcome to Vue CLI" />
+        <footer>
+            <div class="copy-sec pt-5 pb-5 font-weight-bold text-center text-white">
+                <span class="mr-4">CopyRight: Travel Taiwan</span>
+                <span>DesignBy: Tracy</span>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+.copy-sec {
+    font-size: 1.2rem;
+    background-color: #08a6bb;
+    font-family: Roboto, Helvetica, Arial, sans-serif;
 }
+/* #app {
+    font-family: 
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+} */
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="main">
+        <!-- <h1>{{ msg }}</h1>
+        <p>
+            For a guide and recipes on how to configure / customize this project,<br />
+            check out the
+            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a
+            >.
+        </p> -->
+        <select name="#" id="#">
+            <option value="#"></option>
+            <option value="#"></option>
+            <option value="#"></option>
+        </select>
+        <select name="#" id="#">
+            <option value="#"></option>
+            <option value="#"></option>
+            <option value="#"></option>
+        </select>
+        <button>確認</button>
+        <h2 class="pl-4 font-weight-bold">熱門景點</h2>
+        <p class="pl-4 font-weight-bold">
+            台灣的各個美景，都美不勝收。<br />等你一同來發現這座寶島的奧妙！
+        </p>
+        <div class="attract-sec row">
+            <div
+                class="attract-item col-12 col-sm-3 rounded"
+                v-for="item in attractions"
+                :key="item.ID"
+            >
+                <img :src="item.Picture.PictureUrl1" :alt="item.Picture.PictureDescription1" />
+                <h3 class="attract-title font-weight-bold pl-3">{{ item.Name }}</h3>
+                <p class="pl-3">{{ item.City }}</p>
+            </div>
+        </div>
+        <h2 class="pl-4 font-weight-bold">活動類別</h2>
+        <p class="pl-4 font-weight-bold">各種不同的活動內容<br />邀請您一銅來共襄盛舉！</p>
+        <div class="activities-sec row text-center">
+            <div class="activity-item col-12 col-sm-3">年度活動</div>
+            <div class="activity-item col-12 col-sm-3">藝文活動</div>
+            <div class="activity-item col-12 col-sm-3">節慶活動</div>
+            <div class="activity-item col-12 col-sm-3">其他</div>
+        </div>
+    </div>
 </template>
 
 <script>
+import jsSHA from "jssha";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            test: "test",
+            attractions: [],
+        };
+    },
+    methods: {
+        getAuthorizationHeader() {
+            const AppID = "e966312d4abd46fbbe1d4748dd4d71d5";
+            const AppKey = "46Va5HzlLVj6YLoZOXWCGzfBts8";
+            // HMAC 加密演算
+            let GMTString = new Date().toGMTString();
+            let ShaObj = new jsSHA("SHA-1", "TEXT");
+            ShaObj.setHMACKey(AppKey, "TEXT");
+            ShaObj.update("x-date: " + GMTString);
+            let HMAC = ShaObj.getHMAC("B64");
+            let Auth = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
+            // let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+            return { Authorization: Auth, "X-Date": GMTString };
+        },
+        getTDXdata() {
+            const vm = this;
+            const url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=8&$format=JSON`;
+            vm.axios
+                .get(url, { headers: this.getAuthorizationHeader() })
+                .then((response) => {
+                    console.log(response);
+                    vm.attractions = response.data;
+                })
+                .catch(() => {
+                    console.log("failed");
+                });
+        },
+    },
+    created() {
+        this.getTDXdata();
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h2 {
+    font-size: 1.875rem;
+    color: #08a6bb;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+p {
+    color: #aeaeae;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.main {
+    background-color: #e5e5e5;
 }
-a {
-  color: #42b983;
+.attract-title {
+    font-size: 1.2rem;
+}
+.attract-item {
+    background-color: #ffffff;
+}
+.attract-item img {
+    height: 300px;
 }
 </style>
